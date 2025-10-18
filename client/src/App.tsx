f2c8b167b1460cf72cdcc85bf926971dc31cd5e1
tairu07 <tairu07@users.noreply.github.com>
2025-10-18 05:14:45 -0400

    Add Player route to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
+import Player from "./pages/Player";
 import MyPage from "./pages/MyPage";
 import Favorites from "./pages/Favorites";
 
@@ -12,6 +13,7 @@ function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/player"} component={Player} />
       <Route path={"/mypage"} component={MyPage} />
       <Route path={"/favorites"} component={Favorites} />
       <Route path={"/404"} component={NotFound} />
